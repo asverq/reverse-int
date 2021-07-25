a7967c675d638a7886d1796412d8039f7ca9163d
Igor <igor.arin@gmail.com>
2021-07-25 13:53:07 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let res = Math.abs(n);
+    res = res.toString().split("").reverse().join("");
+    return res;
+};
+
